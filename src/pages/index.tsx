5d39b570467f5be33b523c58fc4173421ec1540c
Greg <gskril@gmail.com>
2022-08-19 08:35:40 -0400

    Organize repo and remove boiler plate code

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>lilnouns.eth</title>
+        <meta property="og:title" content="lilnouns.eth" />
+        <meta name="description" content="Claim your lilnouns.eth subdomain" />
+        <meta
+          property="og:description"
+          content="Claim your lilnouns.eth subdomain"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@lilnounsdao" />
+        <meta name="twitter:creator" content="@gregskril" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main></main>
+    </>
+  )
+}
+
+export default Home
