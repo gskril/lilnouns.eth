2c2aec2b829bab687ac6486b28ba02a21650b1a5
Greg <gskril@gmail.com>
2022-09-09 21:41:50 -0400

    Modify select outline on NFT grid

--- src/components/nft-grid.tsx
+++ src/components/nft-grid.tsx
@@ -5,16 +5,14 @@ export default function Gallery({ nfts, tokenId, setTokenId }: NftGrid) {
     <div className="nfts">
       {nfts &&
         nfts.map((nft: Nft) => (
-          <>
-            <div
-              className={'nft' + (nft.tokenId === tokenId ? ' selected' : '')}
-              key={nft.tokenId}
-              onClick={() => setTokenId(nft.tokenId)}
-            >
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img className="nft__image" src={nft.image} alt={nft.name} />
-            </div>
-          </>
+          <div
+            className={'nft' + (nft.tokenId === tokenId ? ' selected' : '')}
+            key={nft.tokenId}
+            onClick={() => setTokenId(nft.tokenId)}
+          >
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img className="nft__image" src={nft.image} alt={nft.name} />
+          </div>
         ))}
     </div>
   )
